27a6e6a66b3b3080f3b19180b3548bfbbfb5e0be
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-04 16:58:44 -0700

    Done

--- miscellaneous/python_tutorials/mobile_number.py
+++ miscellaneous/python_tutorials/mobile_number.py
@@ -5,3 +5,16 @@ Given an integer N, and N phone numbers, print YES or NO to tell if it is a vali
 
 A valid number starts with either 7, 8, or 9, and is 10 digits long.
 """
+
+n = input()
+
+for i in range(n):
+    number = raw_input()
+    if number.isdigit():
+        number = int(number)
+        if (len(str(number)) == 10) and (int(str(number)[0]) in [7,8,9]):
+            print "YES"
+        else:
+            print "NO"
+    else:
+        print "NO"
