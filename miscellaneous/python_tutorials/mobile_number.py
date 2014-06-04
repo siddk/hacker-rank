3df4af0de1b31e3a12c4d4a4a412fada60d846cf
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-04 16:50:39 -0700

    IO stub

---
+++ miscellaneous/python_tutorials/mobile_number.py
@@ -0,0 +1,7 @@
+"""
+mobile_number.py
+
+Given an integer N, and N phone numbers, print YES or NO to tell if it is a valid number or not.
+
+A valid number starts with either 7, 8, or 9, and is 10 digits long.
+"""
