46d6a78c7b21a11dcaf416ef712b3f92af3f9fc1
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-04 15:23:34 -0700

    IO stub

---
+++ miscellaneous/python_tutorials/list_comprehensions.py
@@ -0,0 +1,10 @@
+"""
+list_comprehensions.py
+
+You are given three integers X, Y, and Z denoting the dimensions of a cuboid. You have to print a list of all possible coordinates on the three dimensional grid, such that at any point the sum Xi + Yi + Zi is not equal to N.
+"""
+
+x = input()
+y = input()
+z = input()
+n = input()
