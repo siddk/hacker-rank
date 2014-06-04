d97be41dbd77d36b328b7dac431e23d3b911a698
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-04 16:35:12 -0700

    Preliminary

---
+++ miscellaneous/python_tutorials/name.py
@@ -0,0 +1,10 @@
+"""
+name.py
+
+Given a first and last name, print "Hello firstname lastname. You just delved into python."
+"""
+
+firstname = raw_input()
+lastname = raw_input()
+
+print "Hello %s %s. You just delved into python." % (firstname, lastname)
