1f4b5effcb2e272f479764df08e28f53e7322562
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-04 15:38:51 -0700

    Adding description, io stub

---
+++ miscellaneous/python_tutorials/tuple_switch.py
@@ -0,0 +1,8 @@
+"""
+tuple_switch.py
+
+You are given two integers. Store them into two variables and exchange them. Make sure you use tuple to do the task in one line, rather than using any fancy logic. Print the two numbers correct to two decimal places
+"""
+
+num1 = input()
+num2 = input()
