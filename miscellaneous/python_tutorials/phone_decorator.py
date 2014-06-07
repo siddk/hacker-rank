dffacdabf522a038947c79d2a70ad8e9e33bae68
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-06 18:52:19 -0700

    Testing logic

--- miscellaneous/python_tutorials/phone_decorator.py
+++ miscellaneous/python_tutorials/phone_decorator.py
@@ -9,4 +9,8 @@ count = 0
 record_list = []
 
 while (count < total):
+    phone_number = raw_input()
+    record_list.append(int(phone_number[-10:]))
     count += 1
+
+record_list = sorted(record_list)
