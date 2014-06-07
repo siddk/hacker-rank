7c354f45625581ac42abf5e20a7749c9fa5ec64a
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-06 18:21:36 -0700

    setting iterating component

--- miscellaneous/python_tutorials/decorators2.py
+++ miscellaneous/python_tutorials/decorators2.py
@@ -3,3 +3,9 @@ decorators2.py
 
 Each person has a first name, last name, age, and sex. Print their names in a specific format (Mr./Ms. + First Name + Last Name) sorted by their age in ascending order (youngest first).
 """
+
+number = input()
+count = 0
+
+while(count < number):
+    count++
