15c2907915c09e84fced5c520dc74094f97364b4
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-06 18:39:14 -0700

    IO stub, addition

---
+++ miscellaneous/python_tutorials/calculator.py
@@ -0,0 +1,13 @@
+"""
+calculator.py
+"""
+
+number1 = float(raw_input())
+number2 = float(raw_input())
+
+#addition
+add = number1 + number2
+print "%.2f" % (add)
+
+#
+#print "%.2f" % record_dict[target]
