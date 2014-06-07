c9544b0e4cfdaa0bdb4d90b8cd25c83fbc81ea3b
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-06 18:50:46 -0700

    Loop condition

--- miscellaneous/python_tutorials/phone_decorator.py
+++ miscellaneous/python_tutorials/phone_decorator.py
@@ -3,3 +3,10 @@ phone_decorator.py
 
 Given a series of numbers with optional extensions, sort them in ascending order, print them in desired format.
 """
+
+total = input()
+count = 0
+record_list = []
+
+while (count < total):
+    count += 1
