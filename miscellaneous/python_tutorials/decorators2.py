0be8e316788705318ac1171452e3704a04e66d13
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-06 18:20:51 -0700

    Updating stub

---
+++ miscellaneous/python_tutorials/decorators2.py
@@ -0,0 +1,5 @@
+"""
+decorators2.py
+
+Each person has a first name, last name, age, and sex. Print their names in a specific format (Mr./Ms. + First Name + Last Name) sorted by their age in ascending order (youngest first).
+"""
