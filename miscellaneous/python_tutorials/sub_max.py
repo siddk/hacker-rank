5b3d5f45b64a01d0c6b8ae208bb1f8f4d46fbb9a
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-07 15:03:37 -0700

    preliminary tests

--- miscellaneous/python_tutorials/sub_max.py
+++ miscellaneous/python_tutorials/sub_max.py
@@ -3,3 +3,19 @@ sub_max.py
 
 Find the second largest number in a list.
 """
+
+len_list = int(input())
+str_list = raw_input()
+
+str_list = map(lambda x: int(x), str_list.split())
+
+cur_max = max(str_list)
+
+while(True):
+    str_list.remove(cur_max)
+    if max(str_list) == cur_max:
+        continue
+    else:
+        break
+
+print max(str_list)
