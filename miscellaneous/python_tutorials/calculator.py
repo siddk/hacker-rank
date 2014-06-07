248328d4fd94aea45011f7d4c13fa81309dc5167
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-06 18:40:15 -0700

    Multiplication, division

--- miscellaneous/python_tutorials/calculator.py
+++ miscellaneous/python_tutorials/calculator.py
@@ -9,5 +9,15 @@ number2 = float(raw_input())
 add = number1 + number2
 print "%.2f" % (add)
 
-#
+#subtraction
+sub = number1 = number2
+print "%.2f" % (sub)
+
+#multiplication
+mul = number1 * number2
+print "%.2f" % (mul)
+
+#division
+div = number1 / number2
+print "%.2f" % (div)
 #print "%.2f" % record_dict[target]
