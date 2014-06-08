57540db288115b512be89fd503457e27f678c2f9
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-08 14:35:43 -0700

    Problem stub, actual

--- algorithms/warmups/chocolate_feast.py
+++ algorithms/warmups/chocolate_feast.py
@@ -1,2 +1,5 @@
 """
 chocolate_feast.py
+
+Little Bob loves chocolates, and goes to the store with $N money in his pocket. The price of each chocolate is $C. The store offers a discount: for every M wrappers he gives the store, he’ll get one chocolate for free. How many chocolates does Bob get to eat?
+"""
