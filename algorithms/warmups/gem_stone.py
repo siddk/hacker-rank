9942798d263bd32d8c75f5906c54721198486f5d
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-07 21:23:54 -0700

    Preliminary test

--- algorithms/warmups/gem_stone.py
+++ algorithms/warmups/gem_stone.py
@@ -8,3 +8,20 @@ Given the list of rocks with their compositions, you have to print how many diff
 
 num_test_cases = input()
 cur_case = 0
+intersection_set = set()
+
+while (cur_case < num_test_cases):
+    if (cur_case == 0):
+        gem = raw_input()
+        for element in gem:
+            intersection_set.add(element)
+        cur_case += 1
+    else:
+        gem = raw_input()
+        cur_set = set()
+        for element in gem:
+            cur_set.add(element)
+        intersection_set = intersection_set.intersection(cur_set)
+        cur_case += 1
+
+print len(intersection_set)
