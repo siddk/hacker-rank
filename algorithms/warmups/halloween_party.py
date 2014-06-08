7944aafba38819d20ac7e92daaccdf699cab38cb
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-08 14:23:38 -0700

    IO stub

--- algorithms/warmups/halloween_party.py
+++ algorithms/warmups/halloween_party.py
@@ -3,3 +3,9 @@ halloween_party.py
 
 Alex is attending a Halloween party with his girlfriend Silvia. At the party, Silvia spots a giant chocolate bar. If the chocolate can be served as only 1 x 1 sized pieces and Alex can cut the chocolate bar exactly K times, what is the maximum number of chocolate pieces Alex can cut and give Silvia?
 """
+
+num_test_cases = input()
+current_test_case = 0
+
+while (current_test_case < num_test_cases):
+    current_test_case += 1
