b814263eef6dc7a3d9af8f076e1c13723e49b310
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-08 14:06:21 -0700

    IO stub

--- algorithms/warmups/love_letter.py
+++ algorithms/warmups/love_letter.py
@@ -3,3 +3,9 @@ love_letter.py
 
 James got hold of a love letter that his friend Harry has written for his girlfriend. Being the prankster that James is, he decides to meddle with it. He changes all the words in the letter into palindromes. For any given string, he can only decrease the value of any one of the letters, for example, ‘d’ will become ‘c’, etc. This will count as one operation. (Also, he can decrease the value of letters only till he reaches ‘a’. ‘a’ cannot be further reduced to ‘z’) Find the minimum number of operations he needs to carry out to convert a given string into a palindrome.
 """
+
+num_test_cases = input()
+cur_test_case = 0
+
+while (cur_test_case < num_test_case):
+    cur_test_case += 1
