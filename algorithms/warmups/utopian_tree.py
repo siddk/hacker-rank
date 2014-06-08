af8116e66216caab51f34bcdfe1df45e4a211bca
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-07 20:48:53 -0700

    Adding loop condition

--- algorithms/warmups/utopian_tree.py
+++ algorithms/warmups/utopian_tree.py
@@ -3,3 +3,9 @@ utopian_tree.py
 
 The Utopian tree goes through 2 cycles of growth every year. The first growth cycle of the tree is during the monsoon season when it doubles in height. The second growth cycle is during the summer when it increases in height by 1 meter. If a new Utopian tree sapling of height 1 meter is planted just before the onset of the monsoon season, can you find the height of the tree after N cycles?
 """
+
+num_test_cases = input()
+current_case = 0
+
+while (current_case < num_test_cases):
+    current_case += 1
