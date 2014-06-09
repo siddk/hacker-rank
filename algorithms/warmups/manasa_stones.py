abfbc58401e0de34188158e58117f282029b02c7
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-09 07:47:36 -0700

    Import

--- algorithms/warmups/manasa_stones.py
+++ algorithms/warmups/manasa_stones.py
@@ -3,6 +3,7 @@ manasa_stones.py
 
 Manasa is out on a hike with friends. She finds a trail of stones with numbers on them. She starts following the trail and notices that two consecutive stones have a difference of either a or b. Legend has it that there is a treasure trove at the end of the trail and if Manasa can guess the value of the last stone, the treasure would be hers. Given that the number on the first stone was 0, find all the possible values for the number on the last stone.
 """
+import itertools
 
 num_test_cases = input()
 
