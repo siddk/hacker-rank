429335b8b36a9316ca8133f4e44d69ffb0d8ebbb
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-09 09:35:34 -0700

    problem stub

--- algorithms/warmups/filling_jars.py
+++ algorithms/warmups/filling_jars.py
@@ -1,2 +1,5 @@
 """
 filling_jars.py
+
+Animesh has N empty candy jars, numbered from 1 to N, with infinite capacity. He performs M operations. Each operation is described by 3 integers a, b and k. Here, a and b are index of the jars, and k is the number of candies to be added inside each jar whose index lies between a and b (both inclusive). Can you tell the average number of candies after M operations?
+"""
