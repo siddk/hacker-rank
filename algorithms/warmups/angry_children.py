ee8e4ac080b9ca1c326afa33722c20a80601ae1e
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-10 10:43:14 -0700

    Problem stub

--- algorithms/warmups/angry_children.py
+++ algorithms/warmups/angry_children.py
@@ -1,2 +1,11 @@
 """
 angry_children.py
+
+Bill Gates is on one of his philanthropic journeys to a village in Utopia. He has N packets of candies and would like to distribute one packet to each of the K children in the village (each packet may contain different number of candies). To avoid any fighting among the children, he would like to pick K out of N packets, such that unfairness is minimized.
+
+Suppose the K packets have (x1, x2, x3,….xk) candies in them, where xi denotes the number of candies in the ith packet, then we define unfairness as
+
+max(x1,x2,…xk) - min(x1,x2,…xk)
+
+where max denotes the highest value amongst the elements, and min denotes the least value amongst the elements. Can you figure out the minimum unfairness and print it?
+"""
