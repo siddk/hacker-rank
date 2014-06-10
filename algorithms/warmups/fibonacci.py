afd642cce37c5b83e58deb238f8db7a85bee2fe8
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-10 15:21:22 -0700

    Starting problem 2

--- algorithms/warmups/fibonacci.py
+++ algorithms/warmups/fibonacci.py
@@ -1,2 +1,17 @@
 """
 fibonacci.py
+
+You are given an integer, N. Find out if the number is an element of fibonacci series.
+
+The first few elements of fibonacci series are 0,1,1,2,3,5,8,13…. A fibonacci series is one where every element is a sum of the previous two elements in the series. The first two elements are 0 and 1.
+
+Formally:
+
+fib0 = 0
+fib1 = 1
+fibn = fibn-1 + fibn-2 ∀ n > 1
+"""
+
+#NOTE: I learned earlier in math that a number is only a fibonacci number if 5n^2 + 4 or 5n^2 - 4 is a perfect square, so check for this to get a somewhat optimal solution time.
+
+num_test_cases = input()
