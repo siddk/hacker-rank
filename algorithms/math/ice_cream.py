1d3d9377f258795b40027662bc7d0f042911f3fb
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-14 16:50:01 -0700

    Fixing errors

--- algorithms/math/ice_cream.py
+++ algorithms/math/ice_cream.py
@@ -1,36 +1,38 @@
-"""
-ice_cream.py
+#"""
+#ice_cream.py
 
-Sunny and Johnny together have M dollars which they intend to use at the ice cream parlour. Among N flavors available, they have to choose two distinct flavors whose cost equals M. Given a list of cost of N flavors, output the indices of two items whose sum equals M. The cost of a flavor (ci) will be no more than 10000.
-"""
+#Sunny and Johnny together have M dollars which they intend to use at the ice cream parlour. Among N flavors #available, they have to choose two distinct flavors whose cost equals M. Given a list of cost of N flavors, #output the indices of two items whose sum equals M. The cost of a flavor (ci) will be no more than 10000.
+#"""
 
-num_test_cases = input()
+if __name__ == "__main__":
 
-for case in xrange(num_test_cases):
-    money = input()
-    num_flavors = input()
-    costs = raw_input().split()
+    num_test_cases = input()
 
-    valid_indices = []
-    cost_list = []
-    index_count = 0
+    for case in xrange(num_test_cases):
+        money = input()
+        num_flavors = input()
+        costs = raw_input().split()
 
-    for price in costs:
-        price = int(price)
-        if price < money:
-            valid_indices.append(index_count)
-            cost_list.append(price)
-        else:
-            cost_list.append(price)
+        valid_indices = []
+        cost_list = []
+        index_count = 0
 
-        index_count += 1
+        for price in costs:
+            price = int(price)
+            if price < money:
+                valid_indices.append(index_count)
+                cost_list.append(price)
+            else:
+                cost_list.append(price)
 
-    solution_set = []
+            index_count += 1
 
-    for i in valid_indices:
-        for j in valid_indices[valid_indices.index(i)+1:]:
-            if (cost_list[i] + cost_list[j] == money):
-                solution_set.append((i+1, j+1))
+        solution_set = []
 
-    for sol_tuple in solution_set:
-        print sol_tuple[0], sol_tuple[1]
+        for i in valid_indices:
+            for j in valid_indices[valid_indices.index(i)+1:]:
+                if (cost_list[i] + cost_list[j] == money):
+                    solution_set.append((i+1, j+1))
+
+        for sol_tuple in solution_set:
+            print sol_tuple[0], sol_tuple[1]
